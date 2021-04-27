769a0c433cc7d4f1a1fb198d304103ddb01828fb
Andrzej Lach <lahoo1989@gmail.com>
2021-04-27 13:09:40 +0200

    Added FormatJsonWhitespace method: remove unnecessary whitespaces

--- JsonFormatter.cs
+++ JsonFormatter.cs
@@ -127,6 +127,119 @@ namespace JsonFormatter
             return FormatJson(input);
         }
         
+        /// <summary> Method used for formatting JSON from single-line text to multiline text, easily readable by human. Deletes unnecessary whitespaces. </summary>
+        /// <param name="input"> Raw single-line JSON string; string </param>
+        /// <returns> Formatted, human-readable JSON string </returns>
+        public static string FormatJsonWhitespace(string input)
+        {
+            char[] entering = { '{', '[', '(' };
+            char[] exiting = { '}', ']', ')' };
+            bool inside = false;
+            int level = 0;
+            int letter = 0;
+            string indent = "";
+            var output = new StringBuilder();
+            foreach (char i in input)
+            {
+                if (letter == 0 && i.Equals('{'))
+                {
+                    output.Append(i);
+                    output.Append("\n");
+                    level++;
+                    indent = GetIndent(level);
+                    output.Append(indent);
+                }
+                else if (!inside && i.Equals(' '))
+                {
+                    letter++;
+                    continue;
+                }
+                else if (inside && i.Equals('"'))
+                {
+                    output.Append(i);
+                    inside = false;
+                    //continue;
+                }
+                else if (inside)
+                {
+                    output.Append(i);
+                    //continue;
+                }
+                else if (!inside && i.Equals('"'))
+                {
+                    output.Append(i);
+                    inside = true;
+                    //continue;
+                }
+                else if (entering.Contains(i))
+                {
+                    output.Append("\n");
+                    output.Append(indent);
+                    output.Append(i);
+                    output.Append("\n");
+                    level++;
+                    indent = GetIndent(level);
+                    output.Append(indent);
+                    //continue;
+                }
+                else if (exiting.Contains(i))
+                {
+                    level--;
+                    indent = GetIndent(level);
+                    output.Append("\n");
+                    output.Append(indent);
+                    output.Append(i);
+                    //continue;
+                }
+                else if (i.Equals(','))
+                {
+                    output.Append(i);
+                    output.Append("\n");
+                    output.Append(indent);
+                    //continue;
+                }
+                else if (!inside)
+                {
+                    output.Append(i);
+                }
+                letter++;
+            }
+            return output.ToString();
+        }
+        
+        /// <summary> Method used for formatting JSON from single-line text to multiline text, easily readable by human. Deletes unnecessary whitespaces. </summary>
+        /// <param name="input"> Raw single-line JSON string; string </param>
+        /// <param name="symbol"> Indent symbol; char </param>
+        /// <param name="length"> Single level indent depth </param>
+        /// <returns> Formatted, human-readable JSON string </returns>
+        public static string FormatJsonWhitespace(string input, char symbol, int length)
+        {
+            IndentSymbol = symbol;
+            IndentLength = length;
+            return FormatJson(input);
+        }
+        
+        /// <summary> Method used for formatting JSON from single-line text to multiline text, easily readable by human. Deletes unnecessary whitespaces. </summary>
+        /// <param name="input"> Raw single-line JSON string; string </param>
+        /// <param name="length"> Single level indent depth </param>
+        /// <returns> Formatted, human-readable JSON string </returns>
+        public static string FormatJsonWhitespace(string input, int length)
+        {
+            IndentLength = length;
+            return FormatJson(input);
+        }
+        
+        /// <summary> Method used for formatting JSON from single-line text to multiline text, easily readable by human. Deletes unnecessary whitespaces. </summary>
+        /// <param name="input"> Raw single-line JSON string; string </param>
+        /// <param name="symbol"> Indent symbol; char </param>
+        /// <param name="length"> Single level indent depth </param>
+        /// <returns> Formatted, human-readable JSON string </returns>
+        public static string FormatJsonWhitespace(string input, char symbol)
+        {
+            IndentSymbol = symbol;
+            return FormatJson(input);
+        }
+        
         // set current indent string
         private static string GetIndent(int level)
         {
